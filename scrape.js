3263ff381acadc32c2eaab634d7c852e810d41aa
Allen Hao <allen.hao@wustl.edu>
2016-05-21 10:56:14 -0700

    Play Again
    Play Again button properly functions now

--- scrape.js
+++ scrape.js
@@ -20,8 +20,8 @@ var Player =  mongoose.model('Player', playerSchema);
 request("http://www.basketball-reference.com/leagues/NBA_2016_per_game.html"  , function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
+    db.db.dropCollection('players');
     $('.full_table').each(function() {
-        db.db.dropCollection('players');
         var playerInfo = $(this).find("td:nth-child(2)").html();  
         //Split html to take out name and url fragment of players
         var playerName = playerInfo.split(/[<>]/)[2].replace('&apos;', "'"); //Replace escaped quotes with single quotes
