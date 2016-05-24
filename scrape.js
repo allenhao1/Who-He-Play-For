fa31887fdfec401a5eb799571c10be404a55ddef
Allen Hao <allen.hao@wustl.edu>
2016-05-24 15:02:40 -0700

    Branch into high scores
    Currently, high scores don’t work. Can’t retrieve updated scores due to
    asynchronicity

--- scrape.js
+++ scrape.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var cheerio = require('cheerio');
-var $ = require('jquery');
+
 var picURLFrag = "http://d2cwpp38twqe55.cloudfront.net/req/201604170/images/players/"
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/players');
@@ -15,43 +15,57 @@ var playerSchema = new mongoose.Schema({
   url: String
 })
 var Player =  mongoose.model('Player', playerSchema);
+var urlBase = "http://www.basketball-reference.com/leagues/NBA_";
 
-
-function UrlExists(url, name){
-    request(url, function (error, response, html) {
-      if(error) {
-        console.log("error");
-      }
-      if(!error) {
-        var newPlayer = new Player({
-          name: name,
-          url: url
-        });
-        newPlayer.save(function(err, newPlayer) {
-          if (err) {
-            console.error(err);
-          } else {
-             console.log(newPlayer);
-          }
-        });
-      }
-    });
+function usageMessage(){
+  console.log("node " + process.argv[1] + " [historic year to scrape]");
 }
 
+if(process.argv[3]) {
+  usageMessage();
+} else if (process.argv[2]) {
+  urlBase+= process.argv[2];
+} else {
+  urlBase+= 2016; //Default to current year
+}
+function UrlExists(url, name){
+  request(url, function (error, response, html) {
+    if(!error) {
+     
+    }
+    var query = Player.find({'name': name, 'url': url}, function (err, doc){
+      if(doc.length == 0) {
+        newPlayer.save(function(err, newPlayer) {
+            if (err) {console.error(err);}
+            else { console.log(newPlayer);}
+          });
+      } else {
+        console.log("Player already in database");
+      }
+    });
+  });
+}
 
-request("http://www.basketball-reference.com/leagues/NBA_2016_per_game.html"  , function (error, response, html) {
+request(urlBase + "_per_game.html" , function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
-    db.db.dropCollection('players');
+    //db.db.dropCollection('players'); //We want to add onto, not replace
     $('.full_table').each(function() {
         var playerInfo = $(this).find("td:nth-child(2)").html();  
         //Split html to take out name and url fragment of players
         var playerName = playerInfo.split(/[<>]/)[2].replace('&apos;', "'"); //Replace escaped quotes with single quotes
         var playerURL = playerInfo.split(/[/\.]/)[3];
         var picURL = picURLFrag + playerURL + ".png";
-        // console.log(playerName);
-        // console.log(playerURL);
-        //UrlExists(picURL, playerName);
+        console.log(playerName);
+        console.log(playerURL);
+        var newPlayer = new Player({
+          name: name,
+          url: url
+        });
+        newPlayer.save(function(err, newPlayer) {
+          if (err) {console.error(err);}
+          else { console.log(newPlayer);}
+        });
     });
 
   }
--- server.js
+++ server.js
@@ -9,8 +9,10 @@ var Highscore = db.Highscore;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+var temporaryScores; //Stores high scores for a given session 
 Highscore.find({}).sort({score: 1}).limit(5).exec(function(err, scores) {
 	if (err) {return console.error(err);}
+	temporaryScores = scores;
 	Player.find(function (err, players) {
 	    if (err) {return console.error(err);}
 	    
@@ -27,49 +29,81 @@ Highscore.find({}).sort({score: 1}).limit(5).exec(function(err, scores) {
 			res.json({playerList: players});
 		});
 		app.get('/highscores', function(req, res) {
+			console.log('sending back scores!');
 			console.log(scores);
 			res.json({scores: scores});
+			//res.json({scores: temporaryScores});
 		});
 		app.post('/postScores', function(req, res){
+			var proceed = false; //Manage asynchronicity
 			Highscore.remove({}, function(err) { 
 				console.log("sent over: ");
 				
-				var scores = JSON.parse(req.body.highscores);
-				console.log(scores);
-				//console.log(JSON.parse(req.body));
-				//console.log(req.body);
-				console.log(scores.length);
-				if (scores.length > 1) {
-					console.log("HELLO!");
-					for(var objectIndex in scores) {
-						console.log(objectIndex);
-						console.log(scores[objectIndex]);
+				var newScores = JSON.parse(req.body.highscores);
+				console.log(newScores);
+				temporaryScores = newScores;
+				var check = 1;
+				if (newScores.length > 1) {
+					check = newScores.length;
+					for(var objectIndex in newScores) {
+						console.log("Index: " + objectIndex);
+						console.log(newScores[objectIndex]);
 						//if (scores.hasOwnProperty(object)) {
-							var object = scores[objectIndex];
-							console.log('Object ' + objectIndex);
-							console.log(object);
+							var object = newScores[objectIndex];
+							// console.log('Object ' + objectIndex);
+							// console.log(object);
 						    var score = new Highscore ({
 						    	name: object.name,
 						    	score : object.score
 						    });
 						    score.save(function(err, score) {
-					            if (err) {console.error(err);}
-					            else { console.log("success"); console.log(score);}
-					        });
+					            if (err) {
+					            	console.error(err);
+					            } else { 
+					            	console.log("success"); 
+					            	proceed = true;
+									console.log("go!");
+									console.log(score);
+									check--;
+									console.log('check1: ' + check);
+								}
+							});
 						//}
 					}
+					//while(check != 0){ console.log(check);}
+					console.log('check2:' + check);
+					while(check != 0) { setTimeout(function(){console.log('check3: ' + check)}, 500);}
+					console.log(check)
+					console.log('end');
+
+					
 				} else {
-					var name = Object.keys(scores)[0]
+					var name = Object.keys(newScores)[0]
 					var score = new Highscore ({
 				    	name: name,
 				    	score : scores[name]
 				    });
 				    score.save(function(err, score) {
 			            if (err) {console.error(err);}
-			            else { console.log("success"); console.log(score);}
+			            else { 
+			            	proceed = true;
+			            	console.log("go!");
+			            	console.log("success"); 
+			            	console.log(score);
+			            	check = 0;
+			            }
 			        });
+			        
+			        
 				}
+
 			});
+			
+			// while(!proceed) {
+			// 	//Wait here
+			// 	//console.log("stop!");
+			// }
+			console.log("finished");
 		})
 		//404 Page
 		app.use(function(req,res){
